6eea8577216d1010b6cd87a4c03cd188ac742055
Marcus Brown <marcus.brown1234567@gmail.com>
2024-03-22 15:04:58 -0400

    academic integrity policy updated

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,12 @@
 /********************************************************************************* 
-* WEB422 – Assignment 4
+* WEB422 – Assignment 5
 
 * * I declare that this assignment is my own work in accordance with Seneca's
 * Academic Integrity Policy:
 *
 * https://www.senecapolytechnic.ca/about/policies/academic-integrity-policy.html *
 
-* Name: Marcus Brown Student ID: 127900223 Date: 2024/03/10
+* Name: Marcus Brown Student ID: 127900223 Date: 2024/03/22
 
 *********************************************************************************/
 
